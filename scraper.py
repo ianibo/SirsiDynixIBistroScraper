c8a6d7334589ba6067d7abbab471b9be07e80b53
Ian Ibbotson <ian.ibbotson@gmail.com>
2015-08-15 10:26:54 +0100

    Better exception handling, better processing of content for marc tags

--- scraper.py
+++ scraper.py
@@ -28,7 +28,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 import sys, traceback, logging, shutil, platform
 
@@ -77,12 +77,24 @@ def scrape_catalog_info(driver):
   item_info_tab.click()
   # Do we need to wait for the content?
 
+  print 'Get marc_data table'
   raw_data_table = driver.find_element_by_xpath("//a[@name='marc_data']/following-sibling::table")
 
-  trs = raw_data_table.find_elements(By.TAG_NAME, "tr") 
+  trs = raw_data_table.find_elements(By.TAG_NAME, "tr")
   for row in trs:
-    print row
+    marc_tag = row.find_element_by_xpath("./th")
+    # print "procesing tag %s" % marc_tag.text()
+    indicators = row.find_element_by_xpath("./td[1]")
+    tag_content = row.find_element_by_xpath("./td[2]")
+    print 'Handling content %s' % tag_content.text
+    inner_anchor = tag_content.find_elements_by_xpath("./a")
+    v = None
+    if len(inner_anchor) == 1 :
+      value = inner_anchor[0].text
+    else:
+      v = tag_content.text
 
+    print "Got tag %s indicators %s value %s" % ( marc_tag.text, indicators.text, v )
   return
 
 def scrape_resource_page(driver) :
@@ -90,33 +102,33 @@ def scrape_resource_page(driver) :
   scrape_item_info(driver)
   scrape_catalog_info(driver)
   return
-  
-  
+
+
 def scrape_ibistro() :
   try:
-    print "platform %s" % platform.system() 
-  
+    print "platform %s" % platform.system()
+
     # driver = webdriver.PhantomJS('phantomjs') # or add to your PATH
     driver = webdriver.PhantomJS('./phantomjs_1_9_2_linux_64', service_args=["--webdriver-loglevel=DEBUG"]) # or add to your PATH
     driver.set_window_size(1024, 768) # optional
-  
+
     # The plan is to use this
     # for i in range(ord('a'), ord('n')+1):
     #    print chr(i),
     # To submit a query for every letter and number then iterate the results, indexing by a hash of the identifers [to cope with dups]
     # We'll do the following for each ord - in testing just the a$s  [$ is a wildcard!]
     # Good selenium docs here:: http://selenium-python.readthedocs.org/en/latest/locating-elements.html
-    
+
     # Whack up the debug to see if we can figure out why this throws a Bad Status Line exception when running remotely
     # driver.set_debuglevel(1)
     # Get the front page
     driver.get('http://library.sheffield.gov.uk/uhtbin/webcat')
-    
+
     print 'starting'
-    
+
     # Save screenshot for debug
     driver.save_screenshot('screen_0001.png') # save a screenshot to disk
-    
+
     # Find the search field combo
     searchFieldCombo = Select(driver.find_element_by_name('srchfield1'))
     # Say we want to search by title
@@ -127,51 +139,53 @@ def scrape_ibistro() :
     searchInput.send_keys('a$')
     # Send enter to cause the search to execute
     searchInput.send_keys(Keys.ENTER)
-    
+
     print 'Waiting for first item in results page to appear'
-    
+
     # Wait for the search results page to finish loading
     WebDriverWait(driver, 30).until(
             expected_conditions.presence_of_element_located((By.ID, "VIEW1"))
     )
-    
+
     # Debugging
     driver.save_screenshot('screen_0002.png') # save a screenshot to disk
-    
+
     print 'Clicking button with name VIEW^1'
-    
+
     # Now click the details button for search result 1
     view_record_1_button = driver.find_element_by_name('VIEW^1')
     view_record_1_button.click()
     # Currently blows up here due to problem with phantomjs 1.9.0
-    
+
     print 'Waiting for details page to finish loading'
-    
+
     # Wait for the details page to finish loading
     wait_for_details_page(driver)
-    
+
     print 'got form_type input control.. good to continue'
-    
+
     driver.save_screenshot('screen_0003.png') # save a screenshot to disk
-  
+
     select_full_holidings_and_marc_tags(driver)
-  
+
     driver.save_screenshot('screen_0005.png') # save a screenshot to disk
-  
+
     scrape_resource_page(driver)
-    
-  
+
+
   except:
     print "Unexpected error:", sys.exc_info()
     # logging.exception("Error")
-    traceback.print_exc()
-  
+    # traceback.print_exc()
+    exc_type, exc_value, exc_traceback = sys.exc_info()
+    traceback.print_tb(exc_traceback)
+
   # Eek this is __dirty__ - copy the ghostdriver.log to stdout so it appears on the morph.io screen for easy [easier] debugging
   if dump_ghostdriver_log is True:
     with open("ghostdriver.log", "r") as f:
       shutil.copyfileobj(f, sys.stdout)
-  
-  
+
+
   # Extra notes
   # https://realpython.com/blog/python/headless-selenium-testing-with-python-and-phantomjs/
   return
