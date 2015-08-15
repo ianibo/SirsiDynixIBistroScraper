7de10fb8821fdde98e10cb548230a235245fe74c
Ian Ibbotson <ian.ibbotson@gmail.com>
2015-08-15 02:52:44 +0100

    Added comments in case anyone else looks

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 This is a scraper that runs on [Morph](https://morph.io). To get started [see the documentation](https://morph.io/documentation)
 
+~~~~
 apt-get install libxslt-dev
 apt-get install phantomjs
-
 
 virtualenv --system-site-packages oaf
 source oaf/bin/activate
 pip install -r requirements.txt
-
+~~~~
 
 Run the dev version with 
 
+~~~~
 python scraper.py
+~~~~
 
--- scraper.py
+++ scraper.py
@@ -33,23 +33,47 @@ from selenium.webdriver.support import expected_conditions
 
 driver = webdriver.PhantomJS('phantomjs') # or add to your PATH
 driver.set_window_size(1024, 768) # optional
-driver.get('http://library.sheffield.gov.uk/uhtbin/webcat')
-driver.save_screenshot('screen_0001.png') # save a screenshot to disk
-# sbtn = driver.find_element_by_css_selector('button.gbqfba')
+
+
+
+# The plan is to use this
+# for i in range(ord('a'), ord('n')+1):
+#    print chr(i),
+# To submit a query for every letter and number then iterate the results, indexing by a hash of the identifers [to cope with dups]
+# We'll do the following for each ord - in testing just the a$s  [$ is a wildcard!]
 # Good selenium docs here:: http://selenium-python.readthedocs.org/en/latest/locating-elements.html
+
+# Get the front page
+driver.get('http://library.sheffield.gov.uk/uhtbin/webcat')
+
+# Save screenshot for debug
+driver.save_screenshot('screen_0001.png') # save a screenshot to disk
+
+# Find the search field combo
 searchFieldCombo = Select(driver.find_element_by_name('srchfield1'))
+# Say we want to search by title
 searchFieldCombo.select_by_value('TI^TITLE^SERIES^Title Processing^title')
+# find the search input text control
 searchInput = driver.find_element_by_name('searchdata1')
+# Send the query a$ [Or the ord above]
 searchInput.send_keys('a$')
+# Send enter to cause the search to execute
 searchInput.send_keys(Keys.ENTER)
+
+# Wait for the search results page to finish loading
 WebDriverWait(driver, 30).until(
         expected_conditions.presence_of_element_located((By.ID, "VIEW1"))
 )
+
+# Debugging
 driver.save_screenshot('screen_0002.png') # save a screenshot to disk
 
+# Now click the details button for search result 1
 view_record_1_button = driver.find_element_by_name('VIEW^1')
 view_record_1_button.click()
+# Currently blows up here due to problem with phantomjs 1.9.0
 
+# Wait for the details page to finish loading
 WebDriverWait(driver, 30).until(
         expected_conditions.presence_of_element_located((By.ID, "hiddenlpsubmitdiv"))
 )
