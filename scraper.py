c991709e980ff44d1da5a7d235b9e1a793d71438
Ian Ibbotson <ian.ibbotson@gmail.com>
2015-08-16 18:33:28 +0100

    Merged @otherchirps version using splinter.. attempting run on morph

--- scraper.py
+++ scraper.py
@@ -24,78 +24,56 @@
 # called "data".
 import scraperwiki
 import lxml.html
-from selenium import webdriver
-from selenium import selenium
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions
+from splinter import Browser
 import sys, traceback, logging, shutil, platform
 
 dev_mode = False;
 
-# This might be relevant https://groups.google.com/forum/#!msg/phantomjs/uKTIEYenw78/to4rWFJ8sDgJ
-# This seems to suggest some kind of timeout issue : https://github.com/burnash/gspread/issues/157#issuecomment-53970265
-def wait_for_details_page(driver):
-  print 'Waiting for details page'
-  # Wait for the details page to finish loading
-  WebDriverWait(driver, 15).until(
-    # Looks like presence of is our problem!
-    # expected_conditions.presence_of_element_located((By.NAME, "form_type"))
-    # expected_conditions.visibility_of_element_located((By.CLASS_NAME, "bibinfo"))
-    # Attempt 3 - lets say the page is loaded when the VOPTIONS link is visible
-    expected_conditions.visibility_of_element_located((By.NAME, "VOPTIONS"))
-  )
-  print 'Got full details page'
-  return
 
-
-def select_full_holidings_and_marc_tags(driver):
+def select_full_holidings_and_marc_tags(browser):
   print 'selecting full holdings and marc tags'
-  change_holdings_display_link = driver.find_element_by_name('VOPTIONS')
-  change_holdings_display_link.click()
+  browser.find_by_name('VOPTIONS').first.click()
 
-  # Wait for options page to display
-  WebDriverWait(driver, 30).until(
-    expected_conditions.visibility_of_element_located((By.NAME, "SCROLL^S"))
-  )
+  if not browser.is_element_present_by_name('SCROLL^S', wait_time=30):
+      raise Exception("Failed to find SCROLL^S")
 
-  unformatted_option =  Select(driver.find_element_by_name('vopt_unformatted'))
-  unformatted_option.select_by_value('Y');
-  view_option =  Select(driver.find_element_by_name('vopt_elst'))
-  view_option.select_by_value('ALL');
+  browser.select("vopt_unformatted", "Y")
+  browser.select("vopt_elst", "ALL")
   if dev_mode :
-    driver.save_screenshot('screen_0004.png')
-  ok_button = driver.find_element_by_name('SCROLL^S');
-  ok_button.click();
-  wait_for_details_page(driver);
+    brower.screenshot('screen_0004.png')
+  browser.find_by_name('SCROLL^S').first.click()
+
+  if browser.is_element_present_by_name("VOPTIONS", wait_time=15):
+      print 'Got full details page'
+
   print 'done'
   return
 
-def scrape_item_info(driver):
+def scrape_item_info(browser):
   print 'Getting item info'
   # bibinfo_div = driver.find_element_by_name('bibinfo') - It's class not name
   # title = bibinfo_div.find_element_by
   return
 
-def scrape_catalog_info(driver):
+def scrape_catalog_info(browser):
   print 'Getting catalog info'
-  item_info_tab = driver.find_element_by_id('tab3')
-  item_info_tab.click()
+
+  browser.find_by_id('tab3').first.click()
   # Do we need to wait for the content?
 
   print 'Get marc_data table'
-  raw_data_table = driver.find_element_by_xpath("//a[@name='marc_data']/following-sibling::table")
+  raw_data_table = browser.find_by_xpath(
+      "//a[@name='marc_data']/following-sibling::table"
+  )
 
-  trs = raw_data_table.find_elements(By.TAG_NAME, "tr")
+  trs = raw_data_table.find_by_tag("tr")
   for row in trs:
-    marc_tag = row.find_element_by_xpath("./th")
+    marc_tag = row.find_by_xpath("./th")
     # print "procesing tag %s" % marc_tag.text()
-    indicators = row.find_element_by_xpath("./td[1]")
-    tag_content = row.find_element_by_xpath("./td[2]")
+    indicators = row.find_by_xpath("./td[1]")
+    tag_content = row.find_by_xpath("./td[2]")
     print 'Handling content %s' % tag_content.text
-    inner_anchor = tag_content.find_elements_by_xpath("./a")
+    inner_anchor = tag_content.find_by_xpath("./a")
     v = None
     if len(inner_anchor) == 1 :
       value = inner_anchor[0].text
@@ -105,10 +83,10 @@ def scrape_catalog_info(driver):
     print "Got tag %s indicators %s value %s" % ( marc_tag.text, indicators.text, v )
   return
 
-def scrape_resource_page(driver) :
+def scrape_resource_page(browser) :
   print 'scraping a resource'
-  scrape_item_info(driver)
-  scrape_catalog_info(driver)
+  scrape_item_info(browser)
+  scrape_catalog_info(browser)
   return
 
 def report_module(name):
@@ -123,80 +101,76 @@ def scrape_ibistro() :
   try:
     print "platform %s" % platform.system()
     print "Python ", sys.version_info
-    report_module('selenium');
+    report_module('splinter');
     report_module('scraperwiki');
 
-    # driver = webdriver.PhantomJS('phantomjs') # or add to your PATH
-    # driver = webdriver.PhantomJS('./phantomjs_1_9_2_linux_64', service_args=["--webdriver-loglevel=DEBUG", "--load-images=false"]) # or add to your PATH
-    driver = webdriver.PhantomJS('./phantomjs_2_0_0_linux_64', service_args=["--webdriver-loglevel=DEBUG"]) # or add to your PATH
-    # driver = webdriver.PhantomJS('./phantomjs_1_9_2_linux_64', service_args=["--webdriver-loglevel=DEBUG"]) # or add to your PATH
-    driver.set_window_size(1024, 768) # optional
+    with Browser('phantomjs') as browser:
 
-    # The plan is to use this
-    # for i in range(ord('a'), ord('n')+1):
-    #    print chr(i),
-    # To submit a query for every letter and number then iterate the results, indexing by a hash of the identifers [to cope with dups]
-    # We'll do the following for each ord - in testing just the a$s  [$ is a wildcard!]
-    # Good selenium docs here:: http://selenium-python.readthedocs.org/en/latest/locating-elements.html
+        # The plan is to use this
+        # for i in range(ord('a'), ord('n')+1):
+        #    print chr(i),
+        # To submit a query for every letter and number then iterate the results, indexing by a hash of the identifers [to cope with dups]
+        # We'll do the following for each ord - in testing just the a$s  [$ is a wildcard!]
+        # Good selenium docs here:: http://selenium-python.readthedocs.org/en/latest/locating-elements.html
 
-    # Whack up the debug to see if we can figure out why this throws a Bad Status Line exception when running remotely
-    # driver.set_debuglevel(1)
-    # Get the front page
-    print 'Get front page'
-    driver.get('http://library.sheffield.gov.uk/uhtbin/webcat')
+        # Whack up the debug to see if we can figure out why this throws a Bad Status Line exception when running remotely
+        # driver.set_debuglevel(1)
+        # Get the front page
+        print 'Get front page'
+        browser.visit('http://library.sheffield.gov.uk/uhtbin/webcat')
 
-    print 'starting'
+        print 'starting'
 
-    # Save screenshot for debug
-    if dev_mode :
-      driver.save_screenshot('screen_0001.png') # save a screenshot to disk
+        # Save screenshot for debug
+        if dev_mode :
+            browser.screenshot('screen_0001.png') # save a screenshot to disk
 
-    # Find the search field combo
-    searchFieldCombo = Select(driver.find_element_by_name('srchfield1'))
-    # Say we want to search by title
-    searchFieldCombo.select_by_value('TI^TITLE^SERIES^Title Processing^title')
-    # find the search input text control
-    searchInput = driver.find_element_by_name('searchdata1')
-    # Send the query a$ [Or the ord above]
-    searchInput.send_keys('a$')
-    # Send enter to cause the search to execute
-    searchInput.send_keys(Keys.ENTER)
+        # Find the search field combo
+        # Say we want to search by title
+        browser.select("srchfield1", "TI^TITLE^SERIES^Title Processing^title")
 
-    print 'Waiting for first item in results page to appear'
+        # find the search input text control
+        # Send the query a$ [Or the ord above]
+        browser.fill("searchdata1", "a$")
+        # Send enter to cause the search to execute
+        button = browser.find_by_xpath(
+            '//input[@class="searchbutton" and @value="Search"]'
+        ).first
+        button.click()
 
-    # Wait for the search results page to finish loading
-    WebDriverWait(driver, 30).until(
-            expected_conditions.visibility_of_element_located((By.ID, "VIEW1"))
-    )
+        print 'Waiting for first item in results page to appear'
 
-    # Debugging
-    if dev_mode:
-      driver.save_screenshot('screen_0002.png') # save a screenshot to disk
+        # Wait for the search results page to finish loading
+        if not browser.is_element_present_by_id('VIEW1', wait_time=30):
+            raise Exception('Failed to find VIEW1')
 
-    print 'Clicking button with name VIEW^1'
+        # Debugging
+        if dev_mode:
+            driver.save_screenshot('screen_0002.png') # save a screenshot to disk
 
-    # Now click the details button for search result 1
-    view_record_1_button = driver.find_element_by_name('VIEW^1')
-    view_record_1_button.click()
-    # Currently blows up here due to problem with phantomjs 1.9.0
+        print 'Clicking button with name VIEW^1'
 
-    print 'Waiting for details page to finish loading'
+        # Now click the details button for search result 1
+        browser.find_by_name('VIEW^1').first.click()
+        # Currently blows up here due to problem with phantomjs 1.9.0
 
-    # Wait for the details page to finish loading
-    wait_for_details_page(driver)
+        print 'Waiting for details page to finish loading'
 
-    print 'got form_type input control.. good to continue'
+        # Wait for the details page to finish loading
+        if browser.is_element_present_by_name('VOPTIONS', wait_time=15):
+            print 'Got full details page'
 
-    if dev_mode:
-      driver.save_screenshot('screen_0003.png') # save a screenshot to disk
+        print 'got form_type input control.. good to continue'
 
-    select_full_holidings_and_marc_tags(driver)
+        if dev_mode:
+            driver.save_screenshot('screen_0003.png') # save a screenshot to disk
 
-    if dev_mode:
-      driver.save_screenshot('screen_0005.png') # save a screenshot to disk
+        select_full_holidings_and_marc_tags(browser)
 
-    scrape_resource_page(driver)
+        if dev_mode:
+            driver.save_screenshot('screen_0005.png') # save a screenshot to disk
 
+        scrape_resource_page(browser)
 
   except:
     print "Unexpected error:", sys.exc_info()
